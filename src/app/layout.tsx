030c9f6e7baad4ffb4d5d819f3a9eef9ba009c95
eduardokuritza <eduardo.kuritza@gmail.com>
2025-12-12 17:02:35 -0300

    :bug: fix: background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import "@/scss/main.scss";
 import { Metadata } from "next";
+import { ModalProvider } from "@/provider/ModalProvider/ModalProvider";
 import QueryProvider from "@/provider/QueryProvider/QueryProvider";
 import Footer from "@/components/Footer/Footer";
-import { ModalProvider } from "@/provider/ModalProvider/ModalProvider";
 
 export const metadata: Metadata = {
   title: "Natal Condor - Promoção de Natal",
   description: "",
-  metadataBase: new URL("https://www.condor.com.br/"),
+  metadataBase: new URL("https://www.natal.condor.com.br"),
   openGraph: {
     title: "Natal Condor - Promoção com Prêmios",
     description: ".",
-    url: "https://www.condor.com.br/",
+    url: "https://www.natal.condor.com.br",
     images: [{ url: "/icon.png", width: 582, height: 158, alt: "Natal Condor" }]
   },
   icons: { icon: { sizes: "582x158", url: "/icon.png", type: "image/png" } }
